bd68161ff19cd4022da75f1f05c85a5cefbdc4f2
DmytroYevtushenko <yevtushenkodmytrov@gmail.com>
2025-02-03 17:48:14 +0200

    Add TODO for improving API abstraction

--- epl-squad-finder-api/EplSquadFinder.Application/Abstractions/ApiFootballService/IApiFootballService.cs
+++ epl-squad-finder-api/EplSquadFinder.Application/Abstractions/ApiFootballService/IApiFootballService.cs
@@ -2,6 +2,7 @@
 
 namespace EplSquadFinder.Application.Abstractions.ApiFootballService;
 
+// TODO: Replace with a more business-oriented abstraction instead of an API-specific one.
 public interface IApiFootballService
 {
     Task<List<ApiFootballTeamWrapper>> GetTeamsByCountryAsync(string country);
